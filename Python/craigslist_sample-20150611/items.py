e6c087a5cf91211cb5decb5d1d71cb1ee10fe55d
yoganandaMN <yogananda.MN.1990@gmail.com>
2015-06-11 23:35:34 +0530

    commit on 20150611

---
+++ Python/craigslist_sample-20150611/items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CraigslistSampleItem(scrapy.Item):
+	title = field()
+	link - field()
